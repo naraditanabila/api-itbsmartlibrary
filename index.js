f3d66cf23f12fd122e4030131fbc4a92f67e3c22
naraditanabila <naraditanabila@gmail.com>
2020-04-23 14:30:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Express = require('express')
 const bodyParser = require('body-parser')
-//const cors = require('cors')
+const cors = require('cors')
 const port = process.env.PORT||3000
 const app = Express()
 const Pool = require('pg').Pool
@@ -23,7 +23,7 @@ const peminjamanRouter = require('./api/peminjaman')
 
 
 app.use(bodyParser())
-//app.use(cors())
+app.use(cors())
 
 app.use(anggotaRouter)
 app.use(bukuRouter)
