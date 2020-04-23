ef0fe455d766258d6fa367be875b24d36f00df50
naraditanabila <naraditanabila@gmail.com>
2020-04-24 00:15:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ const peminjamanRouter = require('./api/peminjaman')
 
 app.use(bodyParser())
 app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method == 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELEETE, PATCH');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use(anggotaRouter)
 app.use(bukuRouter)
