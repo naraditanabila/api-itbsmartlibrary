59d2fd01caadcece39061d417d5bafaf6c030b35
naraditanabila <naraditanabila@gmail.com>
2020-04-24 21:45:38 +0700

    one more update

--- api/peminjaman.js
+++ api/peminjaman.js
@@ -14,6 +14,17 @@ const dbPromise = new Pool({
     ssl: true
 })
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method == 'OPTIONS') {
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+      return res.status(200).json({});
+    }
+    next();
+  });
+
 dbPromise.connect()
 //add peminjaman (POST) -- addNewTransaction
 app.post('/peminjaman/add',async(req,res) => {
